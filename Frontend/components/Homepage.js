a825a8a6c0e0313fb0774368e1b9cad0a4621a55
Tianchi Xu <xtc459924276@gmail.com>
2018-09-17 23:44:25 -0400

    set up reach router and add app bar

---
+++ Frontend/components/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import PrimarySearchAppBar from "./PrimarySearchAppBar";
+export default class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <PrimarySearchAppBar />
+      </div>
+    );
+  }
+}
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
-import ReactDOM from "react-dom"
-require('../node_modules/normalize.css/normalize.css')
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Homepage from "../components/Homepage";
+require("../node_modules/normalize.css/normalize.css");
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        CS 307 project
-      </div>
-    )
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Homepage} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
