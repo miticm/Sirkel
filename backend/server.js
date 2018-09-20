cc095ff8f0f62ecc5ee8472abef502ae0d38237f
Tianchi Xu <xtc459924276@gmail.com>
2018-09-20 15:55:02 -0400

    Added mongoDB connection to mlab and created simple User model with email and password

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb://tianchi:sirkel0@ds111063.mlab.com:11063/cs307-database",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
 
 app.listen(5000, () => {
   console.log("http://localhost:5000");
