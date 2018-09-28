564e45a8743c0ddb0186669de153b7e945f0fe60
Daniel Hipskind <dhipskind253@Daniels-MacBook-Pro.local>
2018-09-28 00:43:36 -0400

    Dashboard created

--- Frontend/components/Navbar.js
+++ Frontend/components/Navbar.js
@@ -2,18 +2,14 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+
 export default function NavBar(props) {
   return (
     <div>
-      <AppBar position="static" style={{ backgroundColor: '#60b0f4' }}>
+      <AppBar position="fixed" style={{ backgroundColor: '#60b0f4' }}>
         <Toolbar>
-          <IconButton color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
           <Button color="inherit" className="navButton">
             <Link to="/">Home</Link>
           </Button>
