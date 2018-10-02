05a31017e653f127e819fbae371b9483a42c97c7
Elijah Smith <elijahs2106@gmail.com>
2018-10-01 20:47:08 -0400

    Create events routes

---
+++ backend/routes/events.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
+const config = require("../config/database");
+const User = require("../models/user");
+const Org = require("../models/org");
+const Event = require("../models/event");
+
+const router = express.Router();
+
+router.post("/", passport.authenticate("jwt", { session: false }), (req, res, next) => {
+    if (!req.user) {
+        res.json({
+            success: false,
+            msg: "Must be signed in to create an event",
+        });
+    }
+
+    const newEvent = req.body.event;
+    newEvent.poster = {};
+    newEvent.poster.id = req.user._id;
+    newEvent.poster.username = req.user.username;
+    newEvent.attendees = [];
+    newEvent.attendees.push(newEvent.poster);
+    new Event(newEvent).save((err, event) => {
+        if (err) {
+            res.json({
+                success: false,
+                msg: err,
+            });
+        }
+
+        if (event) {
+            res.json({
+                success: true,
+                event
+            });
+        }
+    });
+});
+
+router.get("/", (req, res, next) => {
+    Event.find({}, (err, events) => {
+        if (err) {
+            res.json({
+                success: false,
+                msg: err,
+            });
+        }
+
+        if (events) {
+            res.json({
+                success: true,
+                events
+            });
+        }
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+    Event.findById(req.params.id, (err, event) => {
+        if (err) {
+            res.json({
+                success: false,
+                msg: err,
+            });
+        }
+        
+        if (!event) {
+            res.json({
+                success: false,
+                msg: 'Event not found.',
+            });
+        }
+        else {
+            res.json({
+                success: true,
+                event
+            });
+        }
+    })
+});
+
+router.put(
+    "/:id",
+    passport.authenticate("jwt", { session: false }),
+    (req, res, next) => {
+        Event.findById(req.params.id, (err, event) => {
+            if (event.poster.id === req.user._id) {
+                const newEvent = req.body.event;
+                Event.findByIdAndUpdate(req.params.id, newEvent, (err, updatedEvent) => {
+                    if (err) {
+                        res.json({
+                            success: false,
+                            msg: err,
+                        });
+                    } else {
+                        res.json({
+                            success: true,
+                            event: updatedEvent
+                        });
+                    }
+                });
+            }
+        });
+});
+
+module.exports = router;
