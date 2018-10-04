f29ab3250418939696d77d09389152fe41efb057
Tianchi Xu <xtc459924276@gmail.com>
2018-10-04 19:26:31 -0400

    Add org page

--- Frontend/components/OrgPage.js
+++ Frontend/components/OrgPage.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 import CreateOrg from "./CreateOrg";
+import OrgList from "./OrgList";
+import axios from "axios";
 export default class OrgPage extends Component {
+  state = {
+    OrgList: []
+  };
+  getOrgList = () => {
+    axios
+      .get("http://127.0.0.1:5000/orgs")
+      .then(res => {
+        console.log(res.data);
+        this.setState({ OrgList: res.data.orgs });
+      })
+      .catch(err => console.log(err));
+  };
+  componentDidMount() {
+    this.getOrgList();
+  }
   render() {
     return (
       <div>
         <CreateOrg />
+        {this.state.OrgList.map(org => {
+          return (
+            <OrgList
+              key={org._id}
+              name={org.name}
+              description={org.description}
+            />
+          );
+        })}
       </div>
     );
   }
