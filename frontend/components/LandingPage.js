65db7cc7bc8fa3e3cc9d15b9c07467facab0b1a7
Tianchi Xu <xtc459924276@gmail.com>
2018-10-08 20:57:13 -0400

    Add typer effect in the landing page

--- frontend/components/LandingPage.js
+++ frontend/components/LandingPage.js
@@ -1,7 +1,60 @@
-import React from "react";
 import "./LandingPage.css";
-export default () => (
-  <div className="LandingPage">
-    <div className="logo" />
-  </div>
-);
+
+import React, { Component } from "react";
+
+export default class LandingPage extends Component {
+  words = [
+    "Meet People",
+    "Find Clubs",
+    "Found Clubs",
+    "Go Advanture",
+    "Study Together"
+  ];
+  txt = "";
+  wordIndex = 0;
+  wait = 1000;
+  isDeleting = false;
+  type() {
+    const currentIndex = this.wordIndex % this.words.length;
+    const fulltxt = this.words[currentIndex];
+    if (this.isDeleting) {
+      //remove a char
+      this.txt = fulltxt.substring(0, this.txt.length - 1);
+    } else {
+      //add a char
+      this.txt = fulltxt.substring(0, this.txt.length + 1);
+    }
+    this.setState({ text: this.txt });
+
+    //speed
+    let typeSpeed = 100;
+    if (this.isDeleting) {
+      typeSpeed /= 4;
+    }
+    if (this.txt === fulltxt && !this.isDeleting) {
+      typeSpeed = this.wait;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.wordIndex++;
+      typeSpeed = this.wait / 2;
+    }
+    setTimeout(() => this.type(), typeSpeed);
+  }
+  componentDidMount() {
+    this.type();
+  }
+  state = {
+    text: ""
+  };
+  render() {
+    return (
+      <div className="LandingPage">
+        <div className="logo" />
+        <h1 className="typer">
+          <span className="text">{this.state.text}</span>
+        </h1>
+      </div>
+    );
+  }
+}
