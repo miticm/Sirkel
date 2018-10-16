4e28a2fd706b85c476c31063d55ce1ccc61b1659
Tianchi Xu <xtc459924276@gmail.com>
2018-10-16 14:47:06 -0400

    Add search user feature

--- frontend/components/UserProfile.js
+++ frontend/components/UserProfile.js
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 import axios from "axios";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-export default class UserProfile extends Component {
+import SearchIcon from "@material-ui/icons/Search";
+import { withStyles } from "@material-ui/core/styles";
+import InputBase from "@material-ui/core/InputBase";
+
+class UserProfile extends Component {
   state = {
-    allUsers: []
+    allUsers: [],
+    filteredUsers: [],
+    search: ""
   };
   componentDidMount() {
     this.getAllUsers();
@@ -15,7 +21,8 @@ export default class UserProfile extends Component {
       .then(res => {
         if (res.data.success) {
           this.setState({
-            allUsers: res.data.users
+            allUsers: res.data.users,
+            filteredUsers: res.data.users
           });
         }
       })
@@ -32,17 +39,30 @@ export default class UserProfile extends Component {
       })
       .catch(err => console.log(err));
   };
+  handleKeyUp = e => {
+    this.setState({ search: e.target.value }, () => {
+      let filtered = this.state.allUsers.filter(user =>
+        user.username.toLowerCase().includes(this.state.search.toLowerCase())
+      );
+      this.setState({ filteredUsers: filtered });
+    });
+  };
   render() {
+    const { classes } = this.props;
     return (
       <div>
-        {this.state.allUsers.map(user => {
+        <div className={classes.border}>
+          <SearchIcon />
+          <InputBase placeholder="search user" onKeyUp={this.handleKeyUp} />
+        </div>
+        {this.state.filteredUsers.map(user => {
           return (
             <Paper key={user._id + Math.random() * 100}>
               <div>
                 <h2>{user.username}</h2>
                 <p>{user.email}</p>
                 <Button
-                  style={{ backgroundColor: "#60b0f4" }}
+                  className={classes.bgc}
                   onClick={() => this.onClick(user._id)}
                 >
                   connect
@@ -66,3 +86,13 @@ export default class UserProfile extends Component {
     );
   }
 }
+const styles = theme => ({
+  bgc: {
+    backgroundColor: "#60b0f4"
+  },
+  border: {
+    border: "1px solid #60b0f4"
+  }
+});
+
+export default withStyles(styles)(UserProfile);
