91bd0da9563bb7d313798cded732b492cd969334
Elijah <elijahs2106@gmail.com>
2018-10-21 13:43:05 -0400

    Add users to chat schema

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -2,12 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ChatSchema = new Schema({
+<<<<<<< HEAD
   members: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User"
     }
   ],
+=======
+  user1: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  },
+  user2: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  },
+>>>>>>> Add users to chat schema
   messages: [
     {
       content: {
