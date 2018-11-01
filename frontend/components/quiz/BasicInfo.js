0d07e869351dc847fe458c7cf1d9746cf298d49d
Elijah <elijahs2106@gmail.com>
2018-11-01 14:47:32 -0400

    Modify basic info to include collegeLevel

--- frontend/components/quiz/BasicInfo.js
+++ frontend/components/quiz/BasicInfo.js
@@ -1,62 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 import Button from "@material-ui/core/Button";
 
-function BasicInfo(props) {
-  let data = {
+class BasicInfo extends Component {
+  state = {
     name: "",
-    age: 0
+    collegeLevel: ""
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
-  let changeName = e => {
-    data.name = e.target.value;
-  };
-  let changeAge = e => {
-    data.age = e.target.value;
-  };
-  let onclick = e => {
-    props.updateBasicInfo(data);
-    props.handleNext();
-  };
-  return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Basic informations
-      </Typography>
-      <Grid container spacing={24}>
-        <Grid item sm={12}>
-          <TextField
-            name="name"
-            label="Your name"
-            fullWidth
-            type="text"
-            onChange={changeName}
-          />
+
+  onclick = e => {
+    this.props.updateBasicInfo({
+      name: this.state.name,
+      collegeLevel: this.state.collegeLevel
+    });
+    this.props.handleNext();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Typography variant="h6" gutterBottom>
+          Basic informations
+        </Typography>
+        <Grid container spacing={24}>
+          <Grid item sm={12}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel>Your Name</InputLabel>
+              <Input
+                name="name"
+                value={this.state.name}
+                onChange={this.onChange}
+                required
+              />
+            </FormControl>
+          </Grid>
+          <Grid item sm={12}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel>College Level</InputLabel>
+              <Select
+                value={this.state.collegeLevel}
+                inputProps={{
+                  name: 'collegeLevel'
+                }}
+                onChange={this.onChange}
+                required
+              >
+                <MenuItem value="Undergraduate">
+                  Undergraduate
+                </MenuItem>
+                <MenuItem value="Graduate">
+                  Graduate
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
         </Grid>
-        <Grid item sm={12}>
-          <TextField
-            name="age"
-            label="Your age"
-            fullWidth
-            type="number"
-            onChange={changeAge}
-          />
-        </Grid>
-      </Grid>
-      <Button
-        variant="contained"
-        color="primary"
-        style={{
-          marginTop: "30px",
-          marginLeft: "450px"
-        }}
-        onClick={onclick}
-      >
-        Next
-      </Button>
-    </React.Fragment>
-  );
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            marginTop: "30px",
+            marginLeft: "450px"
+          }}
+          onClick={this.onclick}
+        >
+          Next
+        </Button>
+      </React.Fragment>
+    );
+  }
 }
 
 export default BasicInfo;
