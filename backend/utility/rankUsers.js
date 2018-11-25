5de26e8eda88afd4b1d43aecddf124cc033c9a53
Elijah <elijahs2106@gmail.com>
2018-11-25 15:56:40 -0500

    Filter out users that are already connected and limit to 10 suggestions

--- backend/utility/rankUsers.js
+++ backend/utility/rankUsers.js
@@ -5,15 +5,31 @@ module.exports = rankUsers = (otherUsers, currentUser) => {
 
   otherUsers.map(user => {
     let score = 0;
+    
     tags.forEach(tag => {
       if (user.survey && user.survey.shortQuestions && user.survey.shortQuestions.tags) {
         score += (user.survey.shortQuestions.tags.toLowerCase().match(new RegExp(tag, "g")) || []).length
       }
     });
+
     user.score = score;
   });
 
-  otherUsers.sort((user1, user2) => user2.score - user1.score);
+  const newUsers = otherUsers.filter((otherUser) => {
+    let isConnection = false;
 
-  return otherUsers;
+    otherUser.connections.forEach(connection => {
+      if (connection.username === currentUser.username) {
+        isConnection = true;
+      }
+    });
+
+    return !isConnection;
+  });
+
+  newUsers.sort((user1, user2) => user2.score - user1.score);
+
+  const topUsers = newUsers.splice(0, 10);
+
+  return topUsers;
 }
